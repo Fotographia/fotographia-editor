87966accd540fa3eba50921cd19c63c88a161d22
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-03-11 15:32:11 +0200

    Import new libraries

--- internal/__init__.py
+++ internal/__init__.py
@@ -2,3 +2,7 @@ import internal.views.hello
 import internal.lib.hello
 
 import internal.views.home
+
+import internal.views.upload_Image
+import internal.views.editor
+import internal.utils.validate_filename
