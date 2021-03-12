60dbd23156c6afbe7a8b5135e187b6ba19c31c2d
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-03-12 12:54:46 +0200

    Fix upload image typo

--- internal/__init__.py
+++ internal/__init__.py
@@ -3,6 +3,6 @@ import internal.lib.hello
 
 import internal.views.home
 
-import internal.views.upload_Image
+import internal.views.upload_image
 import internal.views.editor
 import internal.utils.validate_filename
