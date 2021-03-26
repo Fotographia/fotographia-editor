f70afb49e953133ccbd7351cd913ce809a3195c7
dimprama <dev@dimitrisprama.com>
2021-03-26 14:45:46 +0200

    Added new structure + ui

--- internal/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-UPLOAD_FOLDER = os.path.join(APP_ROOT, "static/images")
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
