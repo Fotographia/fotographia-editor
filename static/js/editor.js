5a06df1e232cc212bdc5579a5247237d4a374b7d
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-04-02 16:30:30 +0300

    Organize structure files

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,15 +1,4 @@
-// Get resolution
-const queryString = window.location.search
-
-fetch(`/api/resolution${queryString}`)
-.then(response => response.json())
-.then(data => {
-    const width = document.getElementById("width")
-    width.innerHTML = data.width
-    
-    const height = document.getElementById("height")
-    height.innerHTML = data.height
+window.onbeforeunload = (() => {
+    // show warning dialog
+    return ""
 })
-.error( err => 
-    console.log(err)
-)
