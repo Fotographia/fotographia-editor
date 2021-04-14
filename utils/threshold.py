fc32edf3140e9cc18c0f4aba1b39ca871f1399ca
sophia-ts <sophia.sts.21@gmail.com>
2021-04-14 14:16:16 +0300

    Add threshold feature

---
+++ utils/threshold.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def threshold(path, values):
+
+    img = cv2.imread(path, 0)
+    thres_value = int(values)
+
+    if thres_value == 0:
+        ret, thres = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    elif thres_value == 1:
+        thres = cv2.adaptiveThreshold(
+            img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2
+        )
+    elif thres_value == 2:
+        thres = cv2.adaptiveThreshold(
+            img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+        )
+
+    cv2.imwrite(path, thres)
