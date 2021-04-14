23a8e08a9484d2e51ae7d40d7e56fa7b87e9314c
sophia-ts <sophia.sts.21@gmail.com>
2021-04-14 14:55:00 +0300

    Add threshold feature

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from utils.grayscale import grayscale
 from utils.gamma_correction import gamma_correction
 from utils.negate import negate
 from utils.flip import flip
+from utils.threshold import threshold
 import json
 import os
 
@@ -128,13 +129,14 @@ def flip_image():
     filename = request.args.get("filename")
 
     path = app.config["UPLOAD_FOLDER"] + "/" + session_id + "/" + filename
-    
+
     data = request.get_json("val")
     select_val = data["val"]
 
     flip(path, select_val)
-    
+
     return jsonify("OK"), 200
+
 
 @app.route("/api/gamma-correction", methods=["POST"])
 def feature_gamma_correction():
@@ -148,7 +150,22 @@ def feature_gamma_correction():
     gamma = float(data["gamma_value"])
 
     gamma_correction(path, gamma)
-    
+
+    return jsonify("OK"), 200
+
+
+@app.route("/api/threshold", methods=["POST"])
+def feature_threshold():
+    session_id = request.args.get("session_id")
+    filename = request.args.get("filename")
+
+    path = app.config["UPLOAD_FOLDER"] + "/" + session_id + "/" + filename
+
+    data = request.get_json("thres_value")
+    values = data["thres_value"]
+
+    threshold(path, values)
+
     return jsonify("OK"), 200
 
 
