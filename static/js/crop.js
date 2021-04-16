6176f3ac81245446669ebd3c7c2ec645f5882355
p18mili1 <p18mili1@ionio.gr>
2021-04-16 14:52:23 +0300

    Add crop.js

---
+++ static/js/crop.js
@@ -0,0 +1,39 @@
+const show_crop = (event) => {
+    event.preventDefault()
+    const form = document.getElementById("crop-form")
+    
+    if (form.style.display === "none") {
+        form.style.display = "block"
+    } else {
+        form.style.display = "none"
+    }
+}
+
+const crop = (event) => {
+    event.preventDefault()
+
+    const X = document.getElementById("X_value").value
+    const Y = document.getElementById("Y_value").value
+    const xOffset = document.getElementById("xOffset_value").value
+    const yOffset = document.getElementById("yOffset_value").value
+
+    const body = JSON.stringify({X,Y,xOffset,yOffset})
+
+    const queryString = window.location.search
+    fetch(`/api/crop${queryString}`,{
+            method: "POST",
+            body,
+            headers:{'content-type': 'application/json'}
+        }
+     )
+    .then(response => {
+        reloadImage()
+        document.getElementById("X").innerHTML = X
+        document.getElementById("Y").innerHTML = Y
+        document.getElementById("xOffset").innerHTML = xOffset
+        document.getElementById("yOffset").innerHTML = yOffset
+    })
+    .error( err => 
+        alert(err)
+    )
+}
