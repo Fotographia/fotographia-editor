9ba6643876fef752e6822597bdc3c5828c440e7b
p18mili1 <p18mili1@ionio.gr>
2021-04-16 13:07:48 +0300

    Add crop section

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from utils.gamma_correction import gamma_correction
 from utils.negate import negate
 from utils.rotate import rotate
 from utils.flip import flip
+from utils.crop import crop
 import json
 import os
 
@@ -161,6 +162,25 @@ def feature_gamma_correction():
     gamma = float(data["gamma_value"])
 
     gamma_correction(path, gamma)
+
+    return jsonify("OK"), 200
+
+
+@app.route("/api/crop", methods=["POST"])
+def crop_func():
+    session_id = request.args.get("session_id")
+    filename = request.args.get("filename")
+
+    path = app.config["UPLOAD_FOLDER"] + "/" + session_id + "/" + filename
+
+    data = request.json
+
+    X = int(data["X"])
+    Y = int(data["Y"])
+    xOffset = int(data["xOffset"])
+    yOffset = int(data["yOffset"])
+
+    crop(path, X, Y, xOffset, yOffset)
 
     return jsonify("OK"), 200
 
