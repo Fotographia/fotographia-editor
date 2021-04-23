7fcce7b889a5a73a95cb3d80704f8715f3745b81
dimprama <dev@dimitrisprama.com>
2021-04-23 18:03:56 +0300

    Added zoom buttons

--- static/js/editor.js
+++ static/js/editor.js
@@ -3,18 +3,34 @@ window.onbeforeunload = (() => {
     return ""
 })
 
-// Get canvas element
-const element = document.querySelector(".zoomable")
+// Variables
+const canvas = document.querySelector(".zoomable")
+const zoomInButton = document.getElementById("zoomIn")
+const zoomOutButton = document.getElementById("zoomOut")
+let x = window.innerWidth / 2;
+let y = window.innerHeight / 2;
 
-// Instantiate panzoom with properties
-panzoom(element,{
+// Instantiate panzoom object with properties
+const pz = panzoom(canvas, {
     bounds: true,
-    zoomSpeed: 0.055, // 6.5% per mouse wheel event
+    zoomSpeed: 0.055,           // 6.5% per mouse wheel event
     maxZoom: 3,
     minZoom: 0.1,
-    smoothScroll: false, // disable momentum
+    smoothScroll: false,        // disable momentum
+    zoomDoubleClickSpeed: 1,    // disable double click zoom
 
     initialX: 500,
     initialY: 200,
     initialZoom: 0.4,
+})
+
+// Controlbar zoom buttons
+zoomInButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    pz.smoothZoom(x, y, 1.2)
+})
+
+zoomOutButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    pz.smoothZoom(x, y, 0.8)
 })
