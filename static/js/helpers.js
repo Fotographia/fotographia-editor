df37988dd913b0c80293522d0c3e513ae100c338
Stavros Panakakis <stavrospanakakis@gmail.com>
2021-05-09 19:25:42 +0300

    Fix export button bug

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -2,6 +2,6 @@
 const reloadImage = () => {
     let src = document.getElementById("img").src
     document.getElementById("img").src = src + "?" + new Date().getTime()
-    // const export_button = document.getElementById("export_button") 
-    // export_button.href = export_button.href + "?" +  new Date().getTime()
+    const export_button = document.getElementById("export_button") 
+    export_button.href = export_button.href + "?" +  new Date().getTime()
 }
